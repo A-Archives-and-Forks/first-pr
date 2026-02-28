1cadd0d9582a3b4884c46415865f7e74bdc06c6a
Andrew Nesbitt <andrewnez@gmail.com>
2026-02-28 08:00:08 +0000

    Remove sass, use compiled CSS directly
    The scss only used basic nesting and a few variables, not worth
    maintaining a sass build pipeline for. The compiled firstpr.css
    is now the source of truth for styles.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
 var gulp        = require('gulp');
 var concat      = require('gulp-concat');
-var sass        = require('gulp-dart-sass');
 var uglify      = require('gulp-uglify');
 
 var paths = {
-  js:     ['js/**.js'],
-  sass: {
-    src: ['css/sass/**.scss'],
-    build: 'css/'
-  }
+  js: ['js/**.js']
 };
 
 function generateJs() {
@@ -18,25 +13,11 @@ function generateJs() {
     .pipe(gulp.dest('.'));
 }
 
-function generateSass() {
-  return gulp.src( paths.sass.src )
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest( paths.sass.build ))
-    .pipe(concat('firstpr.css'))
-    .pipe(gulp.dest('.'));
-}
-
-function build() {
-  return gulp.series(generateJs, generateSass)();
-}
-
 function watch() {
   gulp.watch(paths.js, generateJs)
-  gulp.watch(paths.sass.src, generateSass)
 }
 
 gulp.task('js', generateJs);
-gulp.task('sass', generateSass);
-gulp.task('build', gulp.parallel('js', 'sass'));
+gulp.task('build', generateJs);
 gulp.task('watch', watch);
 gulp.task('default', gulp.series('build', 'watch'));
